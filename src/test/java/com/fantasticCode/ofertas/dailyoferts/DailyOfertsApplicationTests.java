7cf39b5e74dc3fc43b4b36083f0e5293498566a3
Carlos Paredes <00028516@uca.edu.sv>
2019-09-28 14:21:38 -0600

    Iniciando Proyecto

---
+++ src/test/java/com/fantasticCode/ofertas/dailyoferts/DailyOfertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasticCode.ofertas.dailyoferts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyOfertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
