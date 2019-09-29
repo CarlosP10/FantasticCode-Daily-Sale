87a04104dbbe1f271013c3b035fcc29b72c45b04
Esther Lara <00049717@uca.edu.sv>
2019-09-29 14:14:36 -0600

    Fixed Repository, now functional

---
+++ FantasticCode-Daily-Sale/src/com/fantasticCode/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.fantasticCode.controller;
+
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+	static Logger log = Logger.getLogger(MainController.class.getName());
+
+	
+	@RequestMapping(value = "/")
+	public ModelAndView initMain() {
+		ModelAndView mav = new ModelAndView();
+			mav.setViewName("login");
+		return mav;
+	}
+
+}
