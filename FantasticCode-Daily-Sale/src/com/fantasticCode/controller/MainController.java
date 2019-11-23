a1ae0959b9d263b11c47892e0fed8b96a0f2b056
John <00005016@uca.edu.sv>
2019-11-23 15:26:31 -0600

    Bug fix MainController

--- FantasticCode-Daily-Sale/src/com/fantasticCode/controller/MainController.java
+++ FantasticCode-Daily-Sale/src/com/fantasticCode/controller/MainController.java
@@ -59,14 +59,6 @@ public class MainController {
 	}
 	
 	//controlador para acceder a las ofertas
-	
-  @RequestMapping(value = "/offer")
-  public ModelAndView offers() {
-	  ModelAndView mav = new ModelAndView();
-	  List<Offer> ofertas = null;
-	  try {
-		if(offerService.findAll() != null) {
-			ofertas = offerService.findAll();
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ModelAndView login(@RequestParam(value = "username") String username,
